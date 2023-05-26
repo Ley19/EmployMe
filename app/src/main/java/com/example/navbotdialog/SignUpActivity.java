d0c30547fbe1b200da56dd38cd43c80174a4e54a
Rafaelmag3 <rafaelmonrroy045gmail.com>
2023-05-26 00:24:59 -0600

    Interfaces de Modulo Recuperar Contraseña

--- app/src/main/java/com/example/navbotdialog/SignUpActivity.java
+++ app/src/main/java/com/example/navbotdialog/SignUpActivity.java
@@ -2,7 +2,12 @@ package com.example.navbotdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.text.Html;
+
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        TextView signUpRedirectText = findViewById(R.id.signUpRedirectText);
+        String text = "¿Aún no tienes cuenta? <b>Registrate</b>";
+        signUpRedirectText.setText(Html.fromHtml(text));
+        TextView forgetPasswordRedirectText = findViewById(R.id.forgetPasswordRedirectText);
+        String text2 = "<u>Olvide mi contraseña </u>";
+        forgetPasswordRedirectText.setText(Html.fromHtml(text2));
+        forgetPasswordRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, forgetPassword.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/navbotdialog/forgetPassword.java
@@ -0,0 +1,60 @@
+package com.example.navbotdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class forgetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+
+
+        TextView signUpRedirectText = findViewById(R.id.signUpRedirectText);
+        String text = "¿Aún no tienes cuenta? <b>Registrate</b>";
+        signUpRedirectText.setText(Html.fromHtml(text));
+        signUpRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(forgetPassword.this, LoginActivity.class);
+                startActivity(intent);
+
+
+
+            }
+        });
+
+        Button sendButton = findViewById(R.id.send_button);
+        EditText emailEditText = findViewById(R.id.send_email);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+
+                if (!email.isEmpty() && isValidEmail(email)) {
+                    // El campo de correo electrónico no está vacío y tiene una sintaxis válida, navegar a otra ventana
+                    Intent intent = new Intent(forgetPassword.this, verifyCode.class);
+                    startActivity(intent);
+                } else {
+                    // El campo de correo electrónico está vacío o tiene una sintaxis inválida, mostrar un mensaje de error
+                    Toast.makeText(forgetPassword.this, "Por favor, ingresa un correo electrónico válido", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    private boolean isValidEmail(String email) {
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        return email.matches(emailPattern);
+    }
+}
