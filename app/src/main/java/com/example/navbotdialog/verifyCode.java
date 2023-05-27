df2504c3c64ee5eafcd73456b6806ded80fc65be
Rafaelmag3 <rafaelmonrroy045gmail.com>
2023-05-26 20:47:43 -0600

    Corrección de Funcionalidad de Interfaz Login

--- app/src/main/java/com/example/navbotdialog/verifyCode.java
+++ app/src/main/java/com/example/navbotdialog/verifyCode.java
@@ -3,6 +3,8 @@ package com.example.navbotdialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class verifyCode extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class verifyCode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_code);
+
+        TextView forgetText = findViewById(R.id.forgetText);
+        String forgetTextC = "Se ha enviado un código de verificación al correo electronico: <b> correo@correo.com</b>";
+        forgetText.setText(Html.fromHtml(forgetTextC));
     }
 }
